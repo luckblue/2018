f9f09e3554f34e4a389f66c0a77ef4f39305a0d3
luckblue <61861291@qq.com>
2018-12-29 17:39:33 +0800

    1222

--- File_Op/For_File.py
+++ File_Op/For_File.py
@@ -1,1 +1,18 @@
-print('test')
+import os
+import sys
+
+f = open("forwrite.txt", "w+",encoding='utf-8')
+f.write("可以 ，你做的很好！ 6666")  # 此时文件对象在最后一行，如果读取，将读不到数据
+s=f.tell()     # 返回文件对象当前位置
+f.seek(0,0)    # 移动文件对象至第一个字符
+str=f.read()
+print(s,str,len(str))
+
+
+# with open("foo.txt", "wb") as f:
+#     f.write("123")
+
+
+# str = open("foo.txt",  "wb")
+# str.write('123')
+# # print(str.re)
