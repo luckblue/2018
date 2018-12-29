fb79edea78ed5c7478d2e2db612a2429ce4ec151
luckblue <61861291@qq.com>
2018-12-29 14:03:36 +0800

    1222

--- YouZan/Get_Token.py
+++ YouZan/Get_Token.py
@@ -1,26 +1,57 @@
 import requests
 import json
 
-#有赞获取信息
-# url='https://open.youzan.com/oauth/token'
-# client_id = '9a8e4eadb6ae221c09'
-# client_secret = '1b764868f05ffdf4d7bf393bb5a631f9'
-# grant_type = 'silent'
-# kdt_id = '42312280'
-# url2=url+'?client_id='+client_id+'&client_secret='+client_secret+'&grant_type='+grant_type +'&kdt_id='+kdt_id
-# html = requests.get(url2)
-# print(json.loads(html.text)['access_token'])
+#常量
+Youzan_url = 'https://open.youzan.com/oauth/token' #有赞调用地址
+client_id = '9a8e4eadb6ae221c09' #有赞云颁发给开发者的应用ID
+client_secret = '1b764868f05ffdf4d7bf393bb5a631f9' #有赞云颁发给开发者的应用secret
+grant_type = 'silent'  #授与方式（固定为 “silent”）
+kdt_id = '42312280'  #授权给该应用的店铺id，控制台里可查看
+#time ='111'
 
-#公众号获取信息
-APPID = 'wx5fd097be09b290b8'
-AppSecret = '9e9d45176e225bab201e8aac93fa821f'
-access_token=requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+APPID+'&secret='+AppSecret)
-print(json.loads(access_token.text))
-print(json.loads(access_token.text)['access_token'])
+APPID = 'wx5fd097be09b290b8' #公众号APPID
+AppSecret = '9e9d45176e225bab201e8aac93fa821f'  #公众号AppSecret
+Weixin_Url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' #公众号调用地址
+
+    #获取有赞access_token
+def get_youzan_access_token():
+    youzan_data = {"client_id":client_id,"client_secret":client_secret,"grant_type":grant_type,"kdt_id":kdt_id}
+    html_json=json.loads(requests.post(url=Youzan_url,data=youzan_data).text)
+    time = html_json['expires_in']
+    print(time)
+    return html_json['access_token']
+
+#有赞通过手机号获取用户open_id
+def get_youzan_OpenID(mobile):
+    url = "https://open.youzan.com/api/oauthentry/youzan.user.weixin.openid/3.0.0/get"
+    access_token = "72530888b2263c01ad73309b60bd9d9d"
+    data = json.dumps({"country_code":"+86","mobile":mobile})
+    #open_id = requests.get(url=url+'?access_token='+access_token,data=data)
+    # open_id = requests.get("https://open.youzan.com/api/oauthentry/youzan.user.weixin.openid/3.0.0/get"+ '?access_token=' + access_token+"&country_code=+86&mobile=13410376830")
+    url = "https://open.youzan.com/api/oauthentry/youzan.user.weixin.openid/3.0.0/get" + '?access_token=' + access_token + "&mobile=13410376830"
+    open_id = json.loads(requests.get(url).text)["response"]["open_id"]
+    return open_id
 
 
-#案例https://blog.csdn.net/weixin_41004350/article/details/78705415
-mobanid='WEn581UTbPn7sXP-fQFEOokbkNRL1Iy6kYwdihGUoYE'
-data = {"touser":"OPENID","template_id":"3pafHfNdhgDaUDGDb4vIIjK8oVr4olQn06fsN9DzqFM","url":"http://weixin.qq.com/download","topcolor":"#FF0000","data":{"User":{"value":"黄先生","color":"#173177"},"Date":{"value":"06月07日 19时24分","color":"#173177"},"CardNumber":{"value":"0426","color":"#173177"},"Type":{"value":"消费","color":"#173177"},"Money":{"value":"人民币260.00元","color":"#173177"},"DeadTime":{"value":"06月07日19时24分","color":"#173177"},"Left":{"value":"6504.09","color":"#173177"}}}
+    #公众号获取access_token
+def get_weix_access_token():
+    res=requests.get(Weixin_Url+APPID+'&secret='+AppSecret)
+    access_token = json.loads(res.text)['access_token']
+    return  access_token
 
-response = requests.post(url='http://www.baidu.com',data=json.dumps(data))
+
+def send_weixin_mes(mobile):
+    #案例https://blog.csdn.net/weixin_41004350/article/details/78705415
+    mobanid = '3pafHfNdhgDaUDGDb4vIIjK8oVr4olQn06fsN9DzqFM'  #消息模板ID，从公众号后台获取
+    OPENID = get_youzan_OpenID(mobile) #从有赞通过手机查询获取
+    access_token = get_weix_access_token()
+    data = {"touser":OPENID,"template_id":mobanid,"url":"http://www.shes.cn","topcolor":"#FF0000","data":{"first":{"value":"恭喜你购买成功！","color":"#173177"},"keyword1":{"value":"巧克力","color":"#173177"},"keyword2":{"value":"39.8元","color":"#173177"},"keyword3":{"value":"2014年9月22日","color":"#173177"},"keyword4":{"value":"39.8","color":"#173177"},"keyword5":{"value":"0","color":"#173177"},"remark":{"value":"欢迎再次购买！","color":"#173177"}}}
+    response = requests.post(url='https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='+access_token,data=json.dumps(data))
+    print(response.text)
+
+
+if __name__=='__main__':
+    # send_weixin_mes("13410376830")
+    get_youzan_access_token()
+    #print(time)
+
