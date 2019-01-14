87e62d84641f8ff76730539e2bae8391aa3c5ae7
luckblue <61861291@qq.com>
2019-01-14 18:22:41 +0800

    1222

--- EZR/Order_E3_EZR.py
+++ EZR/Order_E3_EZR.py
@@ -1,9 +1,15 @@
-加载主模块
+#加载主模块
 
 api = 'api/pbase/prodcrmupload'
 
-获取数据源
+#获取数据源
 
-写入数据目标
+#写入数据目标
 
-数据处理
+#数据处理
+class e3:
+    def e3_ezr(self):
+        print('999')
+
+str=e3()
+str.e3_ezr()
---
+++ EZR/Order_EZR_E3.py
@@ -0,0 +1,13 @@
+def ppp(i):
+    print(i)
+
+
+def order():
+    i=1
+    while(True):
+        ppp(i)
+        i=i+1
+order()
+
+
+
